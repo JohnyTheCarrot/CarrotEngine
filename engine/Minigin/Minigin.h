c8e2663b2049de719a3b7a4f4cf7d65e21cbf836
JohnyTheCarrot <tuurmartens4@gmail.com>
2024-02-23 19:16:50 +0100

    refactor: move files and rename member variables

---
+++ engine/Minigin/Minigin.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <functional>
+#include <filesystem>
+
+namespace dae {
+    class Minigin final {
+        bool m_Quit{};
+    public:
+        explicit Minigin(const std::filesystem::path &dataPath);
+
+        ~Minigin();
+
+        void Run(const std::function<void()> &load);
+
+        void RunOneFrame();
+
+        Minigin(const Minigin &other) = delete;
+
+        Minigin(Minigin &&other) = delete;
+
+        Minigin &operator=(const Minigin &other) = delete;
+
+        Minigin &operator=(Minigin &&other) = delete;
+    };
+}
