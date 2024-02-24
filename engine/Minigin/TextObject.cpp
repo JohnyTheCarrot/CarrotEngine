4bf28464b3debf93a3c8f10a3306f1fafd970da5
JohnyTheCarrot <tuurmartens4@gmail.com>
2024-02-24 10:01:57 +0100

    fix: resolve resource leak on exception

--- engine/Minigin/TextObject.cpp
+++ engine/Minigin/TextObject.cpp
@@ -13,20 +13,26 @@ dae::TextObject::TextObject(const std::string &text, std::shared_ptr<Font> font)
 }
 
 void dae::TextObject::Update() {
-	if (m_NeedsUpdate) {
-		const SDL_Color color = {255, 255, 255, 255};// only white text is supported now
-		const auto      surf  = TTF_RenderText_Blended(m_Font->GetFont(), m_Text.c_str(), color);
-		if (surf == nullptr) {
-			throw std::runtime_error(std::string("Render text failed: ") + SDL_GetError());
-		}
-		auto texture = SDL_CreateTextureFromSurface(Renderer::GetInstance().GetSDLRenderer(), surf);
-		if (texture == nullptr) {
-			throw std::runtime_error(std::string("Create text texture from surface failed: ") + SDL_GetError());
-		}
-		SDL_FreeSurface(surf);
-		m_TextTexture = std::make_shared<Texture2D>(texture);
-		m_NeedsUpdate = false;
+	if (!m_NeedsUpdate) {
+		return;
 	}
+
+	const SDL_Color color{255, 255, 255, 255};// only white text is supported now
+	const auto      surf = TTF_RenderText_Blended(m_Font->GetFont(), m_Text.c_str(), color);
+	if (surf == nullptr) {
+		throw std::runtime_error(std::string("Render text failed: ") + SDL_GetError());
+	}
+
+	const auto texture = SDL_CreateTextureFromSurface(Renderer::GetInstance().GetSDLRenderer(), surf);
+
+	SDL_FreeSurface(surf);
+
+	if (texture == nullptr) {
+		throw std::runtime_error(std::string("Create text texture from surface failed: ") + SDL_GetError());
+	}
+
+	m_TextTexture = std::make_shared<Texture2D>(texture);
+	m_NeedsUpdate = false;
 }
 
 void dae::TextObject::Render() const {
